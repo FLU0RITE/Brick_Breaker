20d395bf850d81a7942084d244a2d9668892d05c
조형석 <chs9902@naver.com>
2025-10-09 20:24:02 +0900

    feat: gorouter 의존성 추가 및 페이지 이동 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:break_brick/presentation/page/custom_router.dart';
 import 'package:break_brick/presentation/page/start/start_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,12 +10,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    // 먼저 Router 사용을 위해 MaterialApp을 MaterialApp.router로 변경을 해주도록 하자.
+    // 라우터를 등록하고 사용하기 위해 .router 메소드를 사용하는 것이다.
+    // home 옵션 파라미터가 없다.
+    return MaterialApp.router(
+      routerConfig: CustomRouter.router,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: StartPage()
     );
   }
 }
